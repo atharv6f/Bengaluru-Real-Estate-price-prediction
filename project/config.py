6c7cd0180986420b7c3184f38cd2c8787932e21b
atharv6f <66382532+atharv6f@users.noreply.github.com>
2020-09-06 22:40:39 +0400

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,10 +1,13 @@
 import os
+import json
 
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = config('SECRET_KEY')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = config('EMAIL_USER')
+    MAIL_PASSWORD = config('EMAIL_PASS')
